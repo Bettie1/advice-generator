6994f32e4fe4b9e70a18ae4999fa19423a6f65df
Bettie1 <69814092+Bettie1@users.noreply.github.com>
2022-11-18 21:44:33 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("btn");
+const adviceP = document.getElementById("advice");
+const spanS = document.getElementById("span");
+
+getRandomAdvice();
+
+async function getRandomAdvice() {
+  const res = await fetch("https://api.adviceslip.com/advice");
+
+  const data = await res.json();
+  adviceP.innerHTML = data.slip.advice;
+  spanS.innerHTML = data.slip.id;
+  console.log("hello");
+}
+
+btn.addEventListener("click", getRandomAdvice);
